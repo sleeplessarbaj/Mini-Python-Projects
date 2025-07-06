f8f33a27040bd886c243d949721212ae48cdc370
Arbaj <arbaj334alam@gmail.com>
2025-07-06 18:22:29 +0530

    Deleted directory

--- GIF-Python/main.py
+++
@@ -1,9 +0,0 @@
-import imageio.v3 as iio
-
-file_names = ['img1.png', 'img2.png', 'img3.png']
-images = [ ]
-
-for file_name in file_names:
-    images.append(iio.imread(file_name))
-
-iio.imwrite('charmander.gif', images, duration = 200, loop = 0) 
